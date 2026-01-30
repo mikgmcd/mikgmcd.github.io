7841447853590a5a399bc3a147649c4dd29d4eef
mikgmweb <mgmcd04@tutanota.com>
2026-01-30 12:45:36 -0500

    fixed formatting for about and music page

--- app/immutable/chunks/DKAPnWaW.js
+++
@@ -1,1 +0,0 @@
-import{Q as a}from"./CEik9Etn.js";a();
---
+++ app/immutable/chunks/DuohRktc.js
@@ -0,0 +1,1 @@
+import{T as o,R as t,a4 as c,U as l}from"./BOvnTUsd.js";function u(e){throw new Error("https://svelte.dev/e/lifecycle_outside_component")}function r(e){t===null&&u(),c&&t.l!==null?a(t).m.push(e):o(()=>{const n=l(e);if(typeof n=="function")return n})}function a(e){var n=e.l;return n.u??={a:[],b:[],m:[]}}export{r as o};
